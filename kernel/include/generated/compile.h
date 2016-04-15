d67538996eaaac902a6da93699d32c3e16ec9eb4
android <android>
2016-04-15 17:29:46 +0900

    modify for actual build

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Mar 19 11:43:18 JST 2016"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Apr 15 15:50:30 JST 2016"
 #define LINUX_COMPILE_BY "cm10"
 #define LINUX_COMPILE_HOST "cm10"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
